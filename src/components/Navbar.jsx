17860802414e0456a947f5900f8a320b8cf4eb5c
Zenitsu-2112 <kp21122002@gmail.com>
2024-02-12 22:46:02 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import { Stack, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+import SearchBar from "./SearchBar";
+import { logo } from "../utils/constants";
+
+const Navbar = () => (
+  <Stack
+    direction="row"
+    alignItems="center"
+    p={2}
+    sx={{
+      position: "sticky",
+      background: "#000",
+      top: 0,
+      justifyContent: "space-between",
+    }}
+  >
+    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+      <img src={logo} alt="logo" height={45} />{" "}
+      <Typography variant="h4" fontWeight="bold" ml={2} sx={{ color: "white" }}>
+        Youtube Beta
+      </Typography>
+    </Link>
+    <SearchBar />
+  </Stack>
+);
+
+export default Navbar;
